1449b0fbf2071961df1b480ab20cbd8a2f62b71a
tedtalksbits <tedaneblake@gmail.com>
2023-12-17 12:07:28 -0500

    build: 3.0.0

--- electron/repository/Repository.ts
+++ electron/repository/Repository.ts
@@ -2,14 +2,17 @@
 
 type RefetchQuery = string | undefined;
 
-export type CreateArgs<T> = [Partial<T>, RefetchQuery];
-export type DeleteArgs<ID extends number | string> = [Id: ID, RefetchQuery];
-export type UpdateArgs<T, ID extends number | string> = [
-  Id: ID,
-  Partial<T>,
-  RefetchQuery
+export type CreateArgs<T> = [t: Partial<T>, RefetchQuery: RefetchQuery];
+export type DeleteArgs<Id extends number | string> = [
+  Id: Id,
+  RefetchQuery: RefetchQuery
 ];
-export type GetByIdArgs<ID extends number | string> = [Id: ID];
+export type UpdateArgs<T, Id extends number | string> = [
+  Id: Id,
+  t: Partial<T>,
+  RefetchQuery: RefetchQuery
+];
+export type GetByIdArgs<Id extends number | string> = [Id: Id];
 
 export interface CRUDRepository<T, ID extends number | string> {
   createOne: (...args: CreateArgs<T>) => RepositoryReturnType<T>;
